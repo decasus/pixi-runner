7d9ee5a7b2588a7f2dca29b0749c9aaf6d0285a7
a.sorokin <toozexx@gmail.com>
2022-09-13 17:52:36 +0300

    ...

--- components/game/game.jsx
+++ components/game/game.jsx
@@ -1,153 +1,143 @@
 import {useRef, useEffect, useState} from "react";
-import * as PIXI from "pixi.js";
-
-const enemySources = ['images/enemy_1.png', 'images/enemy_2.png'];
-const homeSources = ['images/home-1.png', 'images/home-2.png', 'images/home-3.png', 'images/home-4.png', 'images/home-5.png', 'images/home-6.png', 'images/home-7.png']
+import {EnemyFactory, Hero, House, Wave} from "./src/Game";
+import {Application, Container} from "pixi.js";
 
 const Game = () => {
     const ref = useRef(null);
     const [score, setScore] = useState(0);
 
     useEffect(() => {
-        console.log('test');
-        const app = new PIXI.Application({
-            width: 390,
-            height: 844,
-            backgroundColor: 0x323232,
-            antialias: true
-        });
-
-        ref.current.appendChild(app.view);
-
-        // Hero
-
-        const hero = new PIXI.Sprite.from('images/hero.png');
-        hero.anchor.set(0.5);
-        hero.x = app.screen.width / 2;
-        hero.y = app.screen.height - 100;
-        hero.scale.set(0.7)
-        hero.zIndex = 1;
-        app.stage.addChild(hero);
-
-        // Houses
-
-        for (let i = 0; i < 13; i++) {
-            const home = new PIXI.Sprite.from(homeSources[randomInt(0, 6)]);
-            home.anchor.set(0.5);
-            home.x = 14;
-            home.y = 70*i+1;
-            home.scale.set(0.7);
-            app.stage.addChild(home);
-        }
-
-        for (let i = 0; i < 13; i++) {
-            const home = new PIXI.Sprite.from(homeSources[randomInt(0, 6)]);
-            home.anchor.set(0.5);
-            home.x = 376;
-            home.y = 70*i+1;
-            home.scale.set(0.7);
-            app.stage.addChild(home);
-        }
-
-        // Wave
-
-        const wave = new PIXI.Sprite.from('images/wave.png');
-        hero.anchor.set(0.5);
-        wave.x = 0;
-        wave.y = 750;
-        wave.scale.set(0.5);
-        wave.zIndex = 999;
-        app.stage.addChild(wave);
-
-        const numberOfEnemies = 6
-        const enemies = new PIXI.Container();
-        enemies.zIndex = 0;
-        const enemiesPositions = [90, 160, 240, 320];
-
-
-        function createEnemy(pos, m = randomInt(0, 3)) {
-            const enemy = new PIXI.Sprite.from(enemySources[randomInt(0, 1)]);
-            enemy.anchor.set(0.5);
-            enemy.scale.set(0.5);
-            enemy.x = enemiesPositions[m];
-            enemy.y = -150 * pos;
-            enemy.zIndex = -5;
-            enemies.addChild(enemy);
-        }
-
-        for (let i = 0; i < numberOfEnemies; i++) {
-            createEnemy(i, 0);
-            createEnemy(i, 1);
-            createEnemy(i, 2);
-            createEnemy(i, 3);
-        }
-
-        app.stage.addChild(enemies);
-
-        let touchstartX = 0
-        let touchendX = 0
-        let moveHeroLeft = 0
-        let moveHeroRight = 0
-
-        const checkDirection = () => {
-            if (touchendX < touchstartX) moveHeroLeft = hero.x - 70;
-            if (touchendX > touchstartX) moveHeroRight = hero.x + 70;
-        }
-
-        const touchStart = (e) => {
-            touchstartX = e.changedTouches[0].screenX;
-        }
-        const touchEnd = (e) => {
-            touchendX = e.changedTouches[0].screenX;
-            checkDirection()
-        }
-
-        document.addEventListener("touchstart", touchStart);
-        document.addEventListener("touchend", touchEnd);
-
-        app.ticker.add(gameLoop);
-
-        let speed = 2;
-        let time = 0;
-
-        const heroAnim = setInterval(() => {
-            hero.scale.x = (hero.scale.x === -0.7) ? 0.7 : -0.7;
-        }, 100 * speed);
-
-        function gameLoop(delta) {
-            time += 0.1 * delta;
-
-            if (moveHeroLeft) hero.x > moveHeroLeft ? hero.x -= 10 : moveHeroLeft = 0;
-            if (moveHeroRight) hero.x < moveHeroRight ? hero.x += 10 : moveHeroRight = 0;
-
-            enemies.children.forEach(enemy => {
-                enemy.y += speed * delta;
-                if (rectIntersect(hero, enemy) && enemy.alpha === 1) {
-                    enemy.alpha = 0.2;
-                    setScore(score => score + 1);
-                }
-                if (enemy.y > 900) {
-                    enemies.removeChild(enemy);
-                    createEnemy(1);
-                }
+            const app = new Application({
+                width: 390,
+                height: 844,
+                backgroundColor: 0x323232,
+                antialias: true
             });
-        }
 
-        app.start();
+            ref.current.appendChild(app.view);
 
+            let speed = 4;
+            let time = 0;
 
-        return () => {
-            app.destroy(true, true);
-            clearInterval(heroAnim);
-            document.removeEventListener("touchstart", touchStart);
-            document.removeEventListener("touchend", touchEnd);
-        }
+            const hero = new Hero(235, 720);
+            app.stage.addChild(hero);
+            const heroAnim = hero.animate(speed);
 
-    }, []);
+            for (let i = 0; i < 13; i++) {
+                const house = new House(14, 75 * i + 1)
+                app.stage.addChild(house);
+            }
+
+            for (let i = 0; i < 13; i++) {
+                const house = new House(376, 75 * i + 1)
+                app.stage.addChild(house);
+            }
+
+            app.stage.addChild(new Wave());
+
+            const enemies = new Container();
+            app.stage.addChild(enemies);
+
+            let path = [];
+            const pathSize = 4;
+            let lastRoute = 2;
+
+            function generatePath() {
+                //path = [];
+                let direction = randomInt(0, 1);
+                let route = lastRoute;
+                let line = [];
+                for (let i = 0; i < pathSize; i++) {
+                    line = [0, 0, 0, 0];
+                    line[route] = 1;
+                    if (i === pathSize - 1) {
+                        console.log(route)
+                        if (route === 3) direction ? lastRoute = route : lastRoute = route - 1
+                        if (route === 0) direction ? lastRoute = route + 1 : lastRoute = route
+                        else direction ? lastRoute = route - 1 : lastRoute = route + 1
+                        line[lastRoute] = 1;
+                        console.log(lastRoute);
+                    }
+                    path.push(line);
+                    console.log(line);
+                }
+            }
+
+            generatePath();
+            generatePath();
+            generatePath();
+            generatePath();
+            generatePath();
+            generatePath();
+            generatePath();
+            generatePath();
+            generatePath();
+            generatePath();
+            generatePath();
+            generatePath();
+
+            const enemyFactory = new EnemyFactory();
+            enemyFactory.createEnemies(path, enemies);
+
+            let touchstartX = 0
+            let touchendX = 0
+
+            const checkDirection = () => {
+                if (touchendX < touchstartX && hero.x > 75) hero.moveLeft = hero.x - 80;
+                if (touchendX > touchstartX && hero.x < 315) hero.moveRight = hero.x + 80;
+            }
+
+            const touchStart = (e) => {
+                touchstartX = e.changedTouches[0].screenX;
+            }
+            const touchEnd = (e) => {
+                touchendX = e.changedTouches[0].screenX;
+                checkDirection()
+            }
+
+            document.addEventListener("touchstart", touchStart);
+            document.addEventListener("touchend", touchEnd);
+
+            app.ticker.add(gameLoop);
+
+            function gameLoop(delta) {
+                time += 0.1 * delta;
+
+                if (hero.moveLeft) hero.x > hero.moveLeft ? hero.x -= 10 : hero.moveLeft = 0;
+                if (hero.moveRight) hero.x < hero.moveRight ? hero.x += 10 : hero.moveRight = 0;
+
+                enemies.y += speed * delta;
+                if (enemies.y > 900) {
+                    //enemies.y = 100;
+                    //generatePath();
+                    //enemyFactory.createEnemies(path, enemies);
+                }
+                enemies.children.forEach(enemy => {
+                    //enemy.y += speed/2 * delta;
+                    if (rectIntersect(hero, enemy) && enemy.alpha === 1) {
+                        enemy.alpha = 0.2;
+                        setScore(score => score - 1);
+                    }
+                    //if (enemy.y > 900) enemies.removeChild(enemy)
+                });
+            }
+
+            app.start();
+
+            return () => {
+                app.destroy(true, true);
+                clearInterval(heroAnim);
+                document.removeEventListener("touchstart", touchStart);
+                document.removeEventListener("touchend", touchEnd);
+            }
+
+        }, []
+    )
+    ;
 
     return (
         <div>
-            <div>Ваш счёт: {score}</div>
+            <div>Ваши жизни: {score}</div>
             <div ref={ref}></div>
         </div>
     );
---
+++ components/game/src/Game.js
@@ -0,0 +1,65 @@
+import {Sprite, Texture} from "pixi.js";
+import {enemyPositions, enemySources, houseSources} from "../../../constants/constants";
+
+export class Hero extends Sprite {
+    constructor(x = 0, y = 0) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.anchor.set(0.5);
+        this.scale.set(0.7);
+        this.texture = Texture.from('/images/hero.png');
+        this.moveLeft = 0;
+        this.moveRight = 0;
+    }
+
+    animate(speed) {
+        return setInterval(() => {
+            this.scale.x = (this.scale.x === -0.7) ? 0.7 : -0.7;
+        }, 1000 / speed);
+    }
+}
+
+export class House extends Sprite {
+    constructor(x = 0, y = 0) {
+        super();
+        this.anchor.set(0.5);
+        this.scale.set(0.7);
+        this.x = x;
+        this.y = y;
+        this.texture = Texture.from(houseSources[Math.floor(Math.random() * houseSources.length)]);
+    }
+}
+
+export class Wave extends Sprite {
+    constructor() {
+        super();
+        this.x = 200;
+        this.y = 800;
+        this.anchor.set(0.5);
+        this.scale.set(0.5);
+        this.texture = Texture.from('images/wave.png');
+    }
+}
+
+export class Enemy extends Sprite {
+    constructor(x, y) {
+        super();
+        this.anchor.set(0.5);
+        this.scale.y = 0.5;
+        this.scale.x = [0.5, -0.5][Math.floor(Math.random() * 2)];
+        this.x = x;
+        this.y = y;
+        this.texture = Texture.from(enemySources[Math.floor(Math.random() * enemySources.length)]);
+    }
+}
+
+export class EnemyFactory {
+    createEnemies(path, container) {
+        for (let i = 0; i < path.length; i++) {
+            path[i].forEach((value, index) => {
+                !value && container.addChild(new Enemy(enemyPositions[index], -100 * i));
+            })
+        }
+    }
+}
